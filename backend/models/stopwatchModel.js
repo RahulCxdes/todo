c0a7912a498c1714082c146ae898b8c4e1e1ec50
RahulCxdes <149288066+RahulCxdes@users.noreply.github.com>
2024-09-17 19:09:22 +0530

    Update stopwatchModel.js

--- backend/models/stopwatchModel.js
+++ backend/models/stopwatchModel.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const stopwatchSchema = new mongoose.Schema({
   activityId: { type: mongoose.Schema.Types.ObjectId, ref: 'Activity' },
-  activityName: { type: String }, // Add this field
+  activityName: { type: String },
   minutes: { type: Number, default: 0 },
-  username: { type: String, required: true } // Add this field
+  username: { type: String, required: true },
+  followers: [{ type: String }], // Array of usernames following this user
+  following: [{ type: String }]  // Array of usernames this user is following
 });
 
 module.exports = mongoose.model('Stopwatch', stopwatchSchema);
