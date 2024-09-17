531a558b3c2006733f3324f15296bda1f55dfee0
RahulCxdes <149288066+RahulCxdes@users.noreply.github.com>
2024-09-17 20:01:25 +0530

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -8,6 +8,9 @@ import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Header from './pages/Header';
 import Leaderboard from './pages/Leaderboard';
+import Connect from './pages/Connect'; // Import the Connect page
+// import ActivityInfo from './pages/ActivityInfo';
+import ChatBot from './pages/ChatBot';
 
 // Initialize AuthContext before anything else
 export const AuthContext = createContext();
@@ -36,9 +39,13 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/activities" element={<ActivityProgress />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:username" element={<Profile />} /> {/* Dynamic profile route */}
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/leader" element={<Leaderboard />} />
+          <Route path="/connect" element={<Connect />} /> {/* New Connect page */}
+          {/* <Route path="/activity-info/:activityName" element={<ActivityInfo />} /> Activity info page */}
+          <Route path="/bot" element={<ChatBot/>} /> Activity info page
         </Routes>
       </Router>
     </AuthContext.Provider>
